6db7f4eb22d2209caea9b117e664d6758b8a0c5e
patyfil <patyfil@gmail.com>
2022-08-10 15:58:58 -0300

    add arquivo aula 1

---
+++ 1 - Fundamentos do JavaScript/1. Tipos Primitivos/ex1.js
@@ -0,0 +1,23 @@
+idade = 2;
+nome = "Lara";
+sobrenome = "Ayumi";
+bacharel = false;
+dezAnosDepois = "12";
+
+const nomeCompleto = nome + sobrenome;
+console.log(nomeCompleto)
+
+const resultado = idade + dezAnosDepois;
+console.log(resultado)
+
+console.log(bacharel == 1)
+
+
+
+let input = null;
+
+if (input === null) {
+  console.log("não há informação");
+} else {
+  console.log(input);
+}
