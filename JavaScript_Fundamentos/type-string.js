5923a3ad31de616d874a0a63e65ff4f81e396ff0
patyfil <patyfil@gmail.com>
2022-08-10 14:57:31 -0300

    add arquivos da aula 1

---
+++ JavaScript_Fundamentos/type-string.js
@@ -0,0 +1,18 @@
+const texto1 = "Olá, mundo!";
+const texto2 = "Olá, mundo!";
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = 'O Leo disse "oi"';
+const citacao2 = "O Leo disse 'oi'";
+//console.log(citacao);
+//console.log(citacao2);
+
+const citacao3 = "Meu nome é ";
+const meuNome = "Patricia";
+
+// concatenação (+)
+
+console.log(citacao3 + meuNome)
+
+// template string OU template literal
